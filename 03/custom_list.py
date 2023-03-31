75eec05e7ba97c593ae33bf6ba74bd0e647e0503
zhdv19u186 <studystuff@mail.ru>
2023-03-31 03:12:47 +0300

    hw3 done

--- 03/custom_list.py
+++ 03/custom_list.py
@@ -1,79 +1,66 @@
-
-
 class CustomList(list):
     def __init__(self, items):
         super().__init__(item for item in items if type(item) in [int, float])
-        #self.data = [item for item in items if type(item) in [int, float]]
 
     def __add__(self, other):
-        N_max = len(self)
-        N_min = len(other)
+        len_max = len(self)
+        len_min = len(other)
         swap_flag = False
 
-        if N_max < N_min:
-            N_max, N_min = N_min, N_max
-            swap_flag = True 
+        if len_max < len_min:
+            len_max, len_min = len_min, len_max
+            swap_flag = True
 
-        result = [0] * N_min
-        for i in range(N_min):
+        result = [0] * len_min
+        for i in range(len_min):
             result[i] = self[i] + other[i]
 
-        if N_max > N_min:
-            result.extend(other[N_min:] if swap_flag else self[N_min:])
+        if len_max > len_min:
+            result.extend(other[len_min:] if swap_flag else self[len_min:])
         return CustomList(result)
-    
-    
+
     def __radd__(self, other):
         return self + other
-    
-    
+
     def __sub__(self, other):
-        N_max = len(self)
-        N_min = len(other)
+        len_max = len(self)
+        len_min = len(other)
         swap_flag = False
 
-        if N_max < N_min:
-            N_max, N_min = N_min, N_max
-            swap_flag = True 
+        if len_max < len_min:
+            len_max, len_min = len_min, len_max
+            swap_flag = True
 
-        result = [0] * N_min
-        for i in range(N_min):
-            result[i] = self[i] + other[i]
+        result = [0] * len_min
+        for i in range(len_min):
+            result[i] = self[i] - other[i]
 
-        if N_max > N_min:
-            result.extend(other[N_min:] if swap_flag else self[N_min:])
+        if len_max > len_min:
+            result.extend([-x for x in other[len_min:]] if swap_flag
+                          else self[len_min:])
         return CustomList(result)
-    
-    
+
     def __rsub__(self, other):
-        return self + other
-    
+        temp = self - other
+        return CustomList([-x for x in temp])
+
     def __eq__(self, other):
-        return sum(self.data) == sum(other.data)
-    
+        return sum(self) == sum(other)
+
     def __ne__(self, other):
-        return sum(self.data) != sum(other.data)
+        return sum(self) != sum(other)
 
     def __gt__(self, other):
         return sum(self) > sum(other)
-    
+
     def __ge__(self, other):
-        return sum(self.data) >= sum(other.data)
-    
+        return sum(self) >= sum(other)
+
     def __lt__(self, other):
-        return sum(self.data) < sum(other.data)
-    
+        return sum(self) < sum(other)
+
     def __le__(self, other):
-        return sum(self.data) <= sum(other.data)
-    
+        return sum(self) <= sum(other)
+
     def __str__(self):
-        return f"Items: {[x for x in self]}\nSum: {sum(self)}"
-
-
-print(CustomList([5, 1, 3, 7]) + CustomList([1, 2, 7]))  # CustomList([6, 3, 10, 7])
-print(CustomList([1]) + [2, 5])  # CustomList([3, 5])
-print([2, 5] + CustomList([1]))  # CustomList([3, 5])
-
-print(CustomList([5, 1, 3, 7]) - CustomList([1, 2, 7]))  # CustomList([4, -1, -4, 7])
-print(CustomList([1]) - [2, 5])  # CustomList([-1, -5])
-print([2, 5] - CustomList([1]))  # CustomList([1, 5])
+        return f"Items: {list(self)} Sum: {sum(self)}"
---
+++ 03/test_custom_list.py
@@ -0,0 +1,87 @@
+from unittest import TestCase
+from custom_list import CustomList
+
+
+class TestCustomList(TestCase):
+    def test_add_both_custom(self):
+        list1 = CustomList([5, 1, 3, 7])
+        list2 = CustomList([1, 2, 7])
+        expected = CustomList([6, 3, 10, 7])
+
+        result = list1 + list2
+        self.assertEqual(result, expected)
+
+    def test_add_built_in_and_custom(self):
+        list1 = CustomList([1])
+        list2 = [2, 5]
+        expected = CustomList([3, 5])
+
+        result = list1 + list2
+        self.assertEqual(result, expected)
+
+    def test_radd(self):
+        list1 = [2, 5]
+        list2 = CustomList([1])
+        expected = CustomList([3, 5])
+
+        result = list1 + list2
+        self.assertEqual(result, expected)
+
+    def test_sub_both_custom(self):
+        list1 = CustomList([5, 1, 3, 7])
+        list2 = CustomList([1, 2, 7])
+        expected = CustomList([4, -1, -4, 7])
+
+        result = list1 - list2
+        self.assertEqual(result, expected)
+
+    def test_sub_built_in_and_custom(self):
+        list1 = CustomList([1])
+        list2 = [2, 5]
+        expected = CustomList([-1, -5])
+
+        result = list1 - list2
+        self.assertEqual(result, expected)
+
+    def test_rsub(self):
+        list1 = [2, 5]
+        list2 = CustomList([1])
+        expected = CustomList([1, 5])
+
+        result = list1 - list2
+        self.assertEqual(result, expected)
+
+    def test_equal(self):
+        list1 = CustomList([5, 12, -3, 8])
+        list2 = CustomList([16, -4, 10])
+        self.assertEqual(list1 == list2, True)
+
+    def test_not_equal(self):
+        list1 = CustomList([5, 12, -3, 8])
+        list2 = CustomList([0, 22, 0, 0, 0])
+        self.assertEqual(list1 != list2, False)
+
+    def test_greater(self):
+        list1 = CustomList([5, 14, -3, 8])
+        list2 = CustomList([16, -4, 10, 0, 1])
+        self.assertEqual(list1 > list2, True)
+
+    def test_greater_or_equal(self):
+        list1 = CustomList([22])
+        list2 = CustomList([16, -4, 10])
+        self.assertEqual(list1 >= list2, True)
+
+    def test_less(self):
+        list1 = CustomList([5, 12, -3, 15])
+        list2 = CustomList([16, -4, 10])
+        self.assertEqual(list1 < list2, False)
+
+    def test_less_or_equal(self):
+        list1 = CustomList([5, 12, -3, 5])
+        list2 = CustomList([16, -4, 10])
+        self.assertEqual(list1 <= list2, True)
+
+    def test_str(self):
+        list1 = CustomList([5, 12, -7, 25])
+        expected = "Items: [5, 12, -7, 25] Sum: 35"
+        self.assertEqual(str(list1), expected)
