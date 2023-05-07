1c0bd84f22f3dcc8dae2fd1c6b7db933e92a28d6
zhdv19u186 <studystuff@mail.ru>
2023-05-07 20:59:42 +0300

    Fix hw5

--- 05/lru_cache.py
+++ 05/lru_cache.py
@@ -2,24 +2,19 @@ class LRUCache:
     def __init__(self, limit=42):
         self.cache = {}
         self.limit = limit
-        self.order = {}
-        self.timestamp = 0
 
     def get(self, key):
         try:
-            value = self.cache[key]
-            self.order[key] = self.timestamp
-            self.timestamp += 1
+            value = self.cache.pop(key)
+            self.cache[key] = value
             return value
         except KeyError:
             return None
 
     def set(self, key, value):
+        self.cache.pop(key, None)
         self.cache[key] = value
-        self.order[key] = self.timestamp
-        self.timestamp += 1
 
-        if len(self.order) > self.limit:
-            old_key = min(self.order.keys(), key=lambda x: self.order[x])
-            self.order.pop(old_key)
-            self.cache.pop(old_key)
+        if len(self.cache) > self.limit:
+            first_key = next(iter(self.cache))
+            self.cache.pop(first_key)
